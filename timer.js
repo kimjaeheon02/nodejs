08095e4f84873211f7909817583cc00a3b74e45a
kimjaeheon02 <jhkim8273@naver.com>
2021-01-13 17:49:52 +0900

    update01

---
+++ timer.js
@@ -0,0 +1,20 @@
+const timeout = setTimeout(() => {
+    console.log('1.5초 후 실행');
+}, 1500);
+
+const interval = setInterval(() => {
+    console.log('1초마다 실행');
+}, 1000);
+
+const timeImmediate = setImmediate(() => {
+    console.log('즉시 실행');
+});
+
+const timeout2 = setTimeout(() => {
+    console.log('실행되지 않습니다.');
+}, 3000);
+
+setTimeout(() => {
+    clearTimeout(timeout2);
+    clearTimeout(interval)
+}, 2500);
