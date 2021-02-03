fb5bb2dc1ebba48c18833031f66e95c9704685cb
kimjaeheon02 <jhkim8273@naver.com>
2021-02-03 19:49:19 +0900

    update05

---
+++ JavaScript/Class01.js
@@ -0,0 +1,24 @@
+class Human {
+    constructor(type = 'human'){
+        this.type = type;
+    }
+    static isHuman(human){
+        return human instanceof Human;
+    }
+    breathe() {
+        alert('h-a-a-m');
+    }
+}
+class Zero extends Human{
+    constructor(type, firstName, lastName){
+        super(type);
+        this.firstName = firstName;
+        this.lastName = lastName;
+    }
+    sayName() {
+        super.breathe();
+        alert(`${this.firstName} ${this.lastName}`)
+    }
+}
+const newZero = new Zero('human', 'Zero', 'Kim');
+Human.isHuman(newZero); //true
